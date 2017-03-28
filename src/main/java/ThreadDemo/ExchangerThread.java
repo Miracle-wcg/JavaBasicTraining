6aa69055d249ed16842c3ee96d3de325c64b672a
wcgmiracle@gmail.com <wcg47230554>
2017-03-28 22:51:42 +0800

    Add ExchangerThread.java & SemaphoreDemo.java

---
+++ src/main/java/ThreadDemo/ExchangerThread.java
@@ -0,0 +1,37 @@
+package ThreadDemo;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * Created by chengangw on 28/3/17.
+ */
+public class ExchangerThread extends Thread {
+    private String str;
+    private Exchanger<String> exchanger;
+    private int sleepSecond;
+
+    public ExchangerThread(String str, Exchanger<String> exchanger, int sleepSecond) {
+        this.str = str;
+        this.exchanger = exchanger;
+        this.sleepSecond = sleepSecond;
+    }
+
+    public void run() {
+        try {
+            System.out.println(this.getName() + "启动，原数据为" + str + "，时间为" + System.currentTimeMillis());
+            Thread.sleep(sleepSecond * 1000);
+            str = exchanger.exchange(str);
+            System.out.println(this.getName() + "交换了数据，交换后的数据为" + str + "，时间为" + System.currentTimeMillis());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<String>();
+        ExchangerThread exchangerThread = new ExchangerThread("111", exchanger, 3);
+        ExchangerThread exchangerThread1 = new ExchangerThread("222", exchanger, 2);
+        exchangerThread.start();
+        exchangerThread1.start();
+    }
+}
---
+++ src/main/java/ThreadDemo/SemaphoreDemo.java
@@ -0,0 +1,35 @@
+package ThreadDemo;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by chengangw on 28/3/17.
+ */
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        final Semaphore semaphore = new Semaphore(5);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "获取了信号量，时间为" + System.currentTimeMillis());
+                    Thread.sleep(2000);
+                    System.out.println(Thread.currentThread().getName() + "释放了信号量，**时间为" + System.currentTimeMillis());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            }
+        };
+
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(runnable);
+        }
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+    }
+}
